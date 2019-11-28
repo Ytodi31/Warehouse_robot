28a59dab791fd622e09bcd7e065816e2eca400d8
Gautam Balachandran <47335017+Gautam-Balachandran@users.noreply.github.com>
2019-11-28 15:36:31 -0500

    Creating test cases for testing PID Controller and Perception modules

--- test/Gtest.cpp
+++ test/Gtest.cpp
@@ -1,1 +1,109 @@
+/**
+ * @Author Gautam Balachandran
+ * @file Gtest.cpp
+ * @brief PID controller and Perception class testing
+ *
+ */
+
+#include <gtest/gtest.h>
+#include <tf/transform_broadcaster.h>
+#include "../include/pidController.hpp"
+#include "../include/turtlebotPerception.hpp"
+
+/**
+ * @brief Test case that checks if the calculated Euclidean Distance is correct
+ */
+TEST(PIDControllerTest, EuclideanDistancePass) {
+  PidController pidController;
+  tf::Point currentPosition, desiredPosition;
+  currentPosition.x = 10;
+  currentPosition.y = 10;
+  currentPosition.z = 10;
+  desiredPosition.x = 10;
+  desiredPosition.y = 13;
+  desiredPosition.z = 14;
+  double expectPass = 5;
+  double dist = pidController.euclideanDist(currentPosition, desiredPosition);
+  EXPECT_NEAR(expectPass,dist,0.01);// Should Pass
+}
+
+/**
+ * @brief Test case that checks if the calculated Euclidean Distance is of type double
+ */
+TEST(PIDControllerTest, EuclideanDistanceTypePass) {
+  PidController pidController;
+  tf::Point currentPosition, desiredPosition;
+  currentPosition.x = 10;
+  currentPosition.y = 10;
+  currentPosition.z = 10;
+  desiredPosition.x = 10;
+  desiredPosition.y = 13;
+  desiredPosition.z = 14;
+  double expectPass = 5;
+  double dist = pidController.euclideanDist(currentPosition, desiredPosition);
+  EXPECT_EQ(typeid(dist), typeid(double));;// Should Pass
+}
+
+/**
+ * @brief Test case that checks if the calculated steering angle is correct
+ */
+// TEST CASE COMMENTED OUT SINCE I CAN'T MAKE OUT WHAT THE OUTPUT VALUE SHOULD BE!
+/*TEST(PIDControllerTest, SteeringAnglePass) {
+  PidController pidController;
+  tf::Point trajPosition;
+  double angVel = 30.0;
+  trajPosition.x = 10;
+  trajPosition.y = 10;
+  trajPosition.z = 10;
+  double expectPass = 0;
+  double steeringAng = pidController.setSteeringAng(trajPosition, angVel);
+  EXPECT_NEAR(expectPass,steeringAng,0.01);// Should Pass
+}*/
+
+/**
+ * @brief Test case that checks if the calculated Euclidean Distance is of type double
+ */
+TEST(PIDControllerTest, SteeringAngleTypePass) {
+  PidController pidController;
+  tf::Point trajPosition;
+  double angVel = 30.0;
+  trajPosition.x = 10;
+  trajPosition.y = 10;
+  trajPosition.z = 10;
+  double expectPass = 0;
+  double steeringAng = pidController.setSteeringAng(trajPosition, angVel);
+  EXPECT_EQ(typeid(steeringAng), typeid(double));;// Should Pass
+}
+
+/**
+ * @brief Test case that checks if the type of PID Control parameters are correct
+ */
+TEST(PIDControllerTest, PidTypePass) {
+  PidController pidController;
+  tf::Point trajPosition, desiredPosition;
+  double kP, kI, kD;
+  trajPosition.x = 10;
+  trajPosition.y = 10;
+  trajPosition.z = 10;
+  desiredPosition.x = 10;
+  desiredPosition.y = 13;
+  desiredPosition.z = 14;
+  pidController.calcPID(currentPosition, desiredPosition);
+  kP = pidController.getKP();
+  kD = pidController.getKD();
+  kI = pidController.getKI();
+  EXPECT_EQ(typeid(kP), typeid(double));;// Should Pass
+  EXPECT_EQ(typeid(kD), typeid(double));;// Should Pass
+  EXPECT_EQ(typeid(kI), typeid(double));;// Should Pass
+}
+
+/**
+ * @brief Test case that checks if the calculated Euclidean Distance is correct
+ */
+TEST(PIDControllerTest, EuclideanDistancePass) {
+  TurtlebotPerception turtlebotPerception;
+  bool coll;
+  coll = turtlebotPerception.detectCollision();
+  EXPECT_EQ(typeid(coll), typeid(bool));;// Should Pass
+}
 
---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 28, 2019
+ *      Author: gautam
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+
