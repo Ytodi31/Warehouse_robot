25fc943addf8748e5ca6f3eb1236888abda5cb51
Gautam Balachandran <47335017+Gautam-Balachandran@users.noreply.github.com>
2019-11-28 16:21:41 -0500

    Creating the Mock stub files for PIDController and Perception modules

---
+++ src/turtlebotPerception.cpp
@@ -0,0 +1,18 @@
+/*
+ * turtlebotPerception.cpp
+ *
+ *  Created on: Nov 28, 2019
+ *      Author: gautam
+ */
+
+#include <turtlebotPerception.h>
+
+/**
+ * @brief Mock of the function to detect collision
+ * @param none
+ * @return true if obstacle detected
+ */
+bool turtlebotPerception::detectCollision(){
+  return true; // MOCK VALUE FOR TESTING!
+}
+
