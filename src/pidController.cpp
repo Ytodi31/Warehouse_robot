fc429b459c93d49ff040a6e16870f72514dacb5a
Gautam Balachandran <47335017+Gautam-Balachandran@users.noreply.github.com>
2019-12-02 14:48:58 -0500

    [Task 2.3 & 2.11] : Editing the Mock stub files for PIDController and Perception Modules

--- src/pidController.cpp
+++ src/pidController.cpp
@@ -8,51 +8,174 @@
 #include "pidController.hpp"
 
 /**
- * @brief Getter method for the Propotional gain
- * @param  none
- * @return The propotional gain for the controller
+ * @brief Getter method for the Ros Node
+ * @param none
+ * @return The current node handle for the controller
  */
-double PidController::getKP(){
-  return PidController::kP;
+ros::NodeHandle PidController::getControllerNode() {
+  return PidController::controllerNode;
 }
-
 /**
- * @brief Getter method for the Derivative gain
- * @param  none
- * @return The derivative gain for the controller
- */
-double PidController::getKD(){
-  return PidController::kD;
-}
-
-/**
- * @brief Getter method for the Integral gain
- * @param  none
- * @return The integral gain for the controller
- */
-double PidController::getKI(){
-  return PidController::kI;
-}
-
-/**
- * @brief Mock of the method to calculate the controller parameters
- * @param Trajectory position and current position
+ * @brief Setter method for the Ros Node
+ * @param New Node to be set
  * @return none
  */
-void PidController::calcPID(tf::Point trajPosition, tf::Point currentPosition) {
-  double kP, kD, kI;
-  kP = 0.3;
-  kD = 0.05;
-  kI = 1.02;  //  MOCK VALUES FOR TESTING!
+void PidController::setControllerNode(ros::NodeHandle n) {
+  PidController::controllerNode = n;
+}
+/**
+ * @brief Getter method for the velocity publisher
+ * @param none
+ * @return The current velocity publisher
+ */
+ros::Publisher PidController::getVelocityPub() {
+  return PidController::velocityPub;
+}
+/**
+ * @brief Setter method for the velocity publisher
+ * @param New velocity publisher to be set
+ * @return none
+ */
+void PidController::setVelocityPub(ros::Publisher pub) {
+  PidController::velocityPub = pub;
+}
+/**
+ * @brief Getter method for the position subscriber
+ * @param none
+ * @return The current position subscriber
+ */
+ros::Subscriber PidController::getPositionSub() {
+  return PidController::positionSub;
+}
+/**
+ * @brief Setter method for the position subscriber
+ * @param New position subscriber to be set
+ * @return none
+ */
+void PidController::setPositionSub(ros::Subscriber sub) {
+  PidController::positionSub = sub;
+}
+/**
+ * @brief Getter method for the position
+ * @param none
+ * @return The current position of the turtlebot
+ */
+tf::Point PidController::getPosition() {
+  return PidController::position;
+}
+/**
+ * @brief Setter method for the position
+ * @param New position to be set
+ * @return none
+ */
+void PidController::setPosition(tf::Point pos) {
+  PidController::position = pos;
+}
+/**
+ * @brief Getter method for the orientation
+ * @param none
+ * @return The current orientation of the turtlebot
+ */
+double PidController::getOrientation() {
+  return PidController::orientation;
+}
+/**
+ * @brief Setter method for the orientation
+ * @param New orientation to be set
+ * @return none
+ */
+void PidController::setOrientation(double orient) {
+  PidController::orientation = orient;
+}
+/**
+ * @brief Getter method for the linear velocity
+ * @param none
+ * @return The current linear velocity of the turtlebot
+ */
+double PidController::getLinearVel() {
+  return PidController::linearVel;
+}
+/**
+ * @brief Setter method for the linear velocity
+ * @param New linear velocity to be set
+ * @return none
+ */
+void PidController::setLinearVel(double vel) {
+  PidController::linearVel = vel;
+}
+/**
+ * @brief Getter method for the angular velocity
+ * @param none
+ * @return The current angular velocity of the turtlebot
+ */
+double PidController::getAngularVel() {
+  return PidController::angularVel;
+}
+/**
+ * @brief Setter method for the angular velocity
+ * @param New angular velocity to be set
+ * @return none
+ */
+void PidController::setAngularVel(double angVel) {
+  PidController::angularVel = angVel;
+}
+/**
+ * @brief Getter method for the Propotional gain
+ * @param none
+ * @return The current propotional gain of the controller
+ */
+double PidController::getKP() {
+  return PidController::kP;
+}
+/**
+ * @brief Setter method for the propotional gain
+ * @param New propotional gain to be set
+ * @return none
+ */
+void PidController::setKP(double kP) {
   PidController::kP = kP;
+}
+/**
+ * @brief Getter method for the derivative gain
+ * @param none
+ * @return The current derivative gain of the controller
+ */
+double PidController::getKD() {
+  return PidController::kD;
+}
+/**
+ * @brief Setter method for the derivative gain
+ * @param New derivative gain to be set
+ * @return none
+ */
+void PidController::setKD(double kD) {
   PidController::kD = kD;
+}
+/**
+ * @brief Getter method for the integral gain
+ * @param none
+ * @return The current integral gain of the controller
+ */
+double PidController::getKI() {
+  return PidController::kI;
+}
+/**
+ * @brief Setter method for the integral gain
+ * @param New integral gain to be set
+ * @return none
+ */
+void PidController::setKI(double kI) {
   PidController::kI = kI;
 }
-
+/**
+ * @brief Euclidean distance calculator
+ * @param Current position and desired position
+ * @return Calculated distance
+ */
 /**
  * @brief Mock of the euclidean distance calculator
  * @param Current position and desired position
- * @return calculated distance
+ * @return Calculated distance
  */
 double PidController::euclideanDist(tf::Point currentPosition,
                                     tf::Point desiredPosition) {
@@ -61,12 +184,13 @@ double PidController::euclideanDist(tf::Point currentPosition,
 }
 
 /**
- * @brief Mock of the method to calculate the steering angle for the robot
- * @param Trajectory position and the angular velocity
- * @return calculated steering angle
+ * @brief Mock method to calculate the linear and angular velocity for the robot
+ * @param Current position and desired position
+ * @return Calculated linear and angular velocity
  */
-double PidController::calcSteeringAng(tf::Point trajPosition,
-                                      double angVelocity) {
-  double steeringAng = 30;  // MOCK VALUE FOR TESTING!
-  return steeringAng;
+void PidController::calcVel(tf::Point currentPosition,
+                            tf::Point desiredPosition) {
+  PidController::linearVel = 10.0;  // Mock values for testing
+  PidController::angularVel = 10.0;  // Mock values for testing
+
 }
--- src/turtlebotPerception.cpp
+++ src/turtlebotPerception.cpp
@@ -8,11 +8,102 @@
 #include "turtlebotPerception.hpp"
 
 /**
+ * @brief Getter method for the Ros Node
+ * @param  none
+ * @return The current node handle for the perception
+ */
+ros::NodeHandle TurtlebotPerception::getPerceptionNode() {
+  return TurtlebotPerception::perceptionNode;
+}
+/**
+ * @brief Setter method for the Ros Node
+ * @param  New Node to be set
+ * @return none
+ */
+void TurtlebotPerception::setPerceptionNode(ros::NodeHandle n) {
+  TurtlebotPerception::perceptionNode = n;
+}
+/**
+ * @brief Getter method for the distance publisher
+ * @param  none
+ * @return The current distance publisher
+ */
+ros::Publisher TurtlebotPerception::getDistPub() {
+  return TurtlebotPerception::distPub;
+}
+/**
+ * @brief Setter method for the distance publisher
+ * @param  New distance publisher to be set
+ * @return none
+ */
+void TurtlebotPerception::setDistPub(ros::Publisher pub) {
+  TurtlebotPerception::distPub = pub;
+}
+/**
+ * @brief Getter method for the distance subscriber
+ * @param  none
+ * @return The current distance subscriber
+ */
+ros::Subscriber TurtlebotPerception::getDistSub() {
+  return TurtlebotPerception::distSub;
+}
+/**
+ * @brief Setter method for the distance subscriber
+ * @param  New distance subscriber to be set
+ * @return none
+ */
+void TurtlebotPerception::setDistSub(ros::Subscriber sub) {
+  TurtlebotPerception::distSub = sub;
+}
+/**
+ * @brief Getter method for the collision parameter
+ * @param  none
+ * @return The boolean value for collision
+ */
+bool TurtlebotPerception::getCollide() {
+  return TurtlebotPerception::collide;
+}
+/**
+ * @brief Setter method for the collision parameter
+ * @param boolean value for collision
+ * @return none
+ */
+void TurtlebotPerception::setCollide(bool collision) {
+  TurtlebotPerception::collide = collision;
+}
+/**
+ * @brief Function to get the Laser scan data from the Turtlebot
+ * @param Planar laser range-finder data
+ * @return none
+ */
+void TurtlebotPerception::sensorData(
+    const sensor_msgs::LaserScan::ConstPtr &msg) {
+  // MOCK FUNCTION
+}
+/**
  * @brief Mock of the function to detect collision
  * @param none
  * @return true if obstacle detected
  */
-bool TurtlebotPerception::detectCollision(){
-  return true; // MOCK VALUE FOR TESTING!
+bool TurtlebotPerception::detectCollision() {
+  return true;  // MOCK VALUE FOR TESTING!
+}
+/**
+ * @brief Function to detect the Aruco Marker
+ * @param Frame containing the image to be processed and the markerId
+ *  to be compared with
+ * @return True if the marker detected matches the package required
+ */
+bool TurtlebotPerception::detectArucoMarker(cv::Mat imageFrame,
+                                            double markerId) {
+  return true;  // Mock
+}
+/**
+ * @brief Function to detect the depth of the package for grasping
+ * @param Frame containing the image to be processed
+ * @return The calculated depth of the package
+ */
+double TurtlebotPerception::packageDepth(cv::Mat packageImage) {
+  return 10.0;  // Mock
 }
 
